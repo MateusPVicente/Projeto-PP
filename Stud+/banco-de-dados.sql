b8e7764f77d13c8bd467ff5a2362ea210f7a9d4f
VrRms <vitorramos2003@gmail.com>
2018-08-27 09:31:47 -0300

    Inserção do Banco de Dados
    - Criação das tabelas e foreign keys correspondentes.

---
+++ Stud+/banco-de-dados.sql
@@ -0,0 +1,49 @@
+
+--Banco de dados - Prática Profssional:
+
+ create table Usuario(
+ codUsuario int primary key not null,
+ nomeUsuario varchar(30) not null,
+ dataNascimento datetime not null,
+ senhaUsuario int not null
+ )
+
+ select * from Usuario
+
+ create table Acesso(
+ codAcesso int primary key not null,
+ dataAcesso date not null,
+ horaAcesso time not null,
+ codUsuario int not null,
+ constraint fkcodUsuario foreign key(codUsuario) references Usuario(codUsuario)
+ )
+
+ select * from Acesso
+
+ create table Tarefa(
+ codTarefa int primary key not null,
+ disciplina varchar(50) not null,
+ dataEntrega datetime not null,
+ relevancia varchar(50) not null
+ )
+
+ select * from Tarefa
+
+
+ create table StatusTarefa(
+ codStatus int primary key not null,
+ tipoStatus varchar(15) not null
+ )
+
+ select * from StatusTarefa
+
+ create table Forum(
+ codForum int primary key not null,
+ descricao varchar(100) not null,
+ codUsuario int not null,
+ constraint fkcodUsuario2 foreign key(codUsuario) references Usuario(codUsuario)
+ )
+
+ select * from Forum
+
+
